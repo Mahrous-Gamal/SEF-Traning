6e663e77911843d982dbabf5e38477ddd884dc5d
Mahrous-Gamal <mahrousgamal2001@mail.com>
2024-05-08 01:40:13 +0300

    convert to BrowserRouter

--- src/index.js
+++ src/index.js
@@ -5,18 +5,19 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { HashRouter } from 'react-router-dom';
+import { HashRouter ,BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { Store } from './redux/Store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <HashRouter>
+  
+    <BrowserRouter>
        <Provider store={Store}>
           <App/>
        </Provider>
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
